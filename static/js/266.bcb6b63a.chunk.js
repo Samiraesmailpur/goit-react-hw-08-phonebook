ebaaca758ca74f3f1e87b76bf2a41c8af9b3dd77
Samiraesmailpur <99609667+Samiraesmailpur@users.noreply.github.com>
2023-01-27 16:03:02 +0000

    Deploying to gh-pages from @ Samiraesmailpur/goit-react-hw-08-phonebook@304dde1095c66d5af781fa31f468ffe1fa63b5d9 🚀

---
+++ static/js/266.bcb6b63a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var o=n(184);function r(){return(0,o.jsx)(o.Fragment,{children:(0,o.jsx)("div",{style:{color:"#fff",fontSize:"40px",textAlign:"center",marginTop:"50px",fontWeight:"200"},children:"Welcome to the phonebook, where you can save contacts of your family and friends"})})}}}]);
+//# sourceMappingURL=266.bcb6b63a.chunk.js.map
